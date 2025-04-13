7032b2862f9ea41509c872b3f7fe51a002f70f2c
gauravghodinde <gauravghodinde@gmail.com>
2025-04-13 18:52:49 +0530

    successful modular stuct

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,32 +1,131 @@
+# {
+#   description = "A simple NixOS flake";
+
+#   inputs = {
+#     # NixOS official package source, using the nixos-24.11 branch here
+#     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+#     helix.url = "github:helix-editor/helix/master";
+  
+#   };
+
+#   outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+#     # Please replace my-nixos with your hostname
+#     nixosConfigurations = {
+#       nixos = nixpkgs.lib.nixosSystem {
+#         system = "x86_64-linux";
+#         specialArgs = { inherit inputs; };
+#         modules = [
+#           ./configuration.nix
+#           home-manager.nixosModules.home-manager
+#           {
+#             home-manager.useGlobalPkgs = true;
+#             home-manager.useUserPackages = true;
+#             home-manager.users.gaurav = import ./home.nix;
+#           }
+#         ];
+#       };
+# 	  };
+#   };
+# }
+
+
+
 {
-  description = "A simple NixOS flake";
+  description = "Your new nix config";
 
   inputs = {
-    # NixOS official package source, using the nixos-24.11 branch here
+    # Nixpkgs
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     helix.url = "github:helix-editor/helix/master";
+
+    # You can access packages and modules from different nixpkgs revs
+    # at the same time. Here's an working example:
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    # Also see the 'unstable-packages' overlay at 'overlays/default.nix'.
+
+    # Home manager
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
-    # Please replace my-nixos with your hostname
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    ...
+  } @ inputs: let
+    inherit (self) outputs;
+    # Supported systems for your flake packages, shell, etc.
+    systems = [
+      "aarch64-linux"
+      "i686-linux"
+      "x86_64-linux"
+      "aarch64-darwin"
+      "x86_64-darwin"
+    ];
+    # This is a function that generates an attribute by calling a function you
+    # pass to it, with each system as an argument
+    forAllSystems = nixpkgs.lib.genAttrs systems;
+  in {
+    # Your custom packages
+    # Accessible through 'nix build', 'nix shell', etc
+    packages = forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
+    # Formatter for your nix files, available through 'nix fmt'
+    # Other options beside 'alejandra' include 'nixpkgs-fmt'
+    formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
+
+    # Your custom packages and modifications, exported as overlays
+    overlays = import ./overlays {inherit inputs;};
+    # Reusable nixos modules you might want to export
+    # These are usually stuff you would upstream into nixpkgs
+    nixosModules = import ./modules/nixos;
+    # Reusable home-manager modules you might want to export
+    # These are usually stuff you would upstream into home-manager
+    homeManagerModules = import ./modules/home-manager;
+
+    # NixOS configuration entrypoint
+    # Available through 'nixos-rebuild --flake .#your-hostname'
     nixosConfigurations = {
-		nixos = nixpkgs.lib.nixosSystem {
-			system = "x86_64-linux";
-			specialArgs = { inherit inputs; };
-			modules = [
-				./configuration.nix
-				home-manager.nixosModules.home-manager
-				{
-					home-manager.useGlobalPkgs = true;
-					home-manager.useUserPackages = true;
-					home-manager.users.gaurav = import ./home.nix;
-				}
-			];
-		};
-	};
+      # FIXME replace with your hostname
+      your-hostname = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs outputs;};
+        modules = [
+          # > Our main nixos configuration file <
+          ./nixos/configuration.nix
+        ];
+      };
+    };
+   nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        # system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [
+         ./nixos/configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.gaurav = import ./home-manager/home.nix;
+            home-manager.backupFileExtension = "backup";
+          }
+        ];
+      };
+	  };
+
+    # Standalone home-manager configuration entrypoint
+    # Available through 'home-manager --flake .#your-username@your-hostname'
+    # homeConfigurations = {
+    #   # FIXME replace with your username@hostname
+    #   "your-username@your-hostname" = home-manager.lib.homeManagerConfiguration {
+    #     pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+    #     extraSpecialArgs = {inherit inputs outputs;};
+    #     modules = [
+    #       # > Our main home-manager configuration file <
+    #       ./home-manager/home.nix
+    #     ];
+    #   };
+    # };
   };
 }
