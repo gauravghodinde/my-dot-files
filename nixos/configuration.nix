0b42e2efdb5cc64804a94dd363bd60a3cf5e4e16
gauravghodinde <gauravghodinde@gmail.com>
2025-04-13 18:33:34 +0530

    working flakes

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }: 
+{ config, pkgs, inputs, ... }: 
 {
   imports =
     [ # Include the results of the hardware scan.
@@ -171,8 +171,8 @@
     gnumake42
     obsidian
     libclang
-   # ciscoPacketTracer8
-    teamviewer
+    inputs.helix.packages."${pkgs.system}".helix
+ 
     firefox-devedition-bin 
   ];
   services.teamviewer.enable = true;
---
+++ nixos/flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "A simple NixOS flake";
+
+  inputs = {
+    # NixOS official package source, using the nixos-24.11 branch here
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    helix.url = "github:helix-editor/helix/master";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    # Please replace my-nixos with your hostname
+    nixosConfigurations = {
+		nixos = nixpkgs.lib.nixosSystem {
+			system = "x86_64-linux";
+			specialArgs = { inherit inputs; };
+			modules = [
+				./configuration.nix
+				home-manager.nixosModules.home-manager
+				{
+					home-manager.useGlobalPkgs = true;
+					home-manager.useUserPackages = true;
+					home-manager.users.gaurav = import ./home.nix;
+				}
+			];
+		};
+	};
+  };
+}
